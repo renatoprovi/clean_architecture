609b5a1e23626491aa5fed083f5932735835e832
renatoprovi <renato.cruz@provi.com.br>
2025-01-03 10:14:21 -0300

    limpando arquivos

--- referencia/src/domain/__seedwork/use_case_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-
-class UseCaseInterface(ABC):
-
-    @abstractmethod
-    def execute(input: Any) -> Any:
-        raise NotImplementedError
--- referencia/src/infrastructure/api/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings, Field
-
-
-class Settings(BaseSettings):
-    CONNECTION: str = Field(..., env="CONNECTION")
-
-
-settings = Settings()
